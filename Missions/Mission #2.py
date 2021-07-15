22b0059ba72a88f5542c08e5c3ece08fe92f114f
FatherOfJumong <87500968+FatherOfJumong@users.noreply.github.com>
2021-07-16 01:10:39 +0400

    Add files via upload

---
+++ Missions/Mission #2.py
@@ -0,0 +1,15 @@
+ 
+#Computing the minimum number of changing coins
+def min_coin_change(amount):
+    cents = reversed([1,5,10,20,50]) #tetrebi
+    c = 0 #variable to store the minimum number of changing coins
+    for cent in cents:
+        while amount >= cent:  #here we subract the greatest cent we have in our list(cents) until the amount is no longer
+            amount-=cent       #greater than the cent itself, so we can determine the minimum
+            c+=1
+    return c
+
+
+
+
+  
